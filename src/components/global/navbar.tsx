d95d69cc5c3934cb6f173350f626c153c5e85d00
Emre Taş <75886362+emretasss@users.noreply.github.com>
2024-07-05 00:19:38 +0300

    navbartsx

--- src/components/global/navbar.tsx
+++ src/components/global/navbar.tsx
@@ -4,7 +4,9 @@ type Props = {}
 
 const Navbar = async (props: Props) => {
   return (
-    <div>Navbar</div>
+            <header className='fixed right-0 left-0 top-0 py-4 px-4 bg-black/40 backdrop-blur-lg z-[100] flex items-center border-b-[1px] border-neutral-900 justify-between'>
+
+            </header>
   )
 }
 
