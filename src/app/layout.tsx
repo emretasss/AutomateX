46cb3fe0f296298fbd78e7792674e85283d08b1c
Emre Taş <75886362+emretasss@users.noreply.github.com>
2024-07-04 23:27:18 +0300

    layout dark mod

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,7 +16,16 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+      <ThemeProvider
+            attribute="class"
+            defaultTheme="system"
+            enableSystem
+            disableTransitionOnChange
+          >
+            {children}
+          </ThemeProvider>
+      </body>
     </html>
   );
 }
