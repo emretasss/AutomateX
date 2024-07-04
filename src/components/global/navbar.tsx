1ec36667ee63a989364d183a8efb474fcfedc290
Emre Taş <75886362+emretasss@users.noreply.github.com>
2024-07-05 01:04:55 +0300

    navbar link

--- src/components/global/navbar.tsx
+++ src/components/global/navbar.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import Image from '../../../node_modules/next/image'
-
+import Link from '../../../node_modules/next/link'
 type Props = {}
 
 const Navbar = async (props: Props) => {
@@ -17,6 +17,28 @@ const Navbar = async (props: Props) => {
       />
       <p className="text-3xl font-bold">matex</p>
     </aside>
+    <nav className="absolute left-[50%] top-[50%] transform translate-x-[-50%] translate-y-[-50%] hidden md:block">
+        <ul className="flex items-center gap-4 list-none">
+          <li>
+            <Link href="#">Products</Link>
+          </li>
+          <li>
+            <Link href="#">Pricing</Link>
+          </li>
+          <li>
+            <Link href="#">Clients</Link>
+          </li>
+          <li>
+            <Link href="#">Resources</Link>
+          </li>
+          <li>
+            <Link href="#">Documentation</Link>
+          </li>
+          <li>
+            <Link href="#">Enterprise</Link>
+          </li>
+        </ul>
+      </nav>
             </header>
 
             
