98eab9f32bb416690656c676c78045ac3c792510
Emre Taş <75886362+emretasss@users.noreply.github.com>
2024-07-05 00:51:35 +0300

    navbar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,10 @@
+import Navbar from "@/components/global/navbar";
+
 
 export default function Home() {
   return (
    <main>
-    
+      <Navbar/>
    </main>
   );
 }
--- src/components/global/navbar.tsx
+++ src/components/global/navbar.tsx
@@ -4,9 +4,13 @@ type Props = {}
 
 const Navbar = async (props: Props) => {
   return (
-            <header className='fixed right-0 left-0 top-0 py-4 px-4 bg-black/40 backdrop-blur-lg z-[100] flex items-center border-b-[1px] border-neutral-900 justify-between'>
-
+            <header 
+            className="fixed right-0 left-0 top-0 py-4 px-4 bg-black/40 backdrop-blur-lg z-[100] flex items-center border-b-[1px] border-neutral-900 justify-between"
+            >
+                <aside className="flex items-center gap-[2px]"></aside>
             </header>
+
+            
   )
 }
 
