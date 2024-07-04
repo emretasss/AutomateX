def1509ba6086cc07e6b597ea7269956153cd1ce
Emre Taş <75886362+emretasss@users.noreply.github.com>
2024-07-05 01:04:04 +0300

    public and navbar

--- src/components/global/navbar.tsx
+++ src/components/global/navbar.tsx
@@ -1,13 +1,22 @@
 import React from 'react'
+import Image from '../../../node_modules/next/image'
 
 type Props = {}
 
 const Navbar = async (props: Props) => {
   return (
-            <header 
-            className="fixed right-0 left-0 top-0 py-4 px-4 bg-black/40 backdrop-blur-lg z-[100] flex items-center border-b-[1px] border-neutral-900 justify-between"
-            >
-                <aside className="flex items-center gap-[2px]"></aside>
+    <header className="fixed right-0 left-0 top-0 py-4 px-4 bg-black/40 backdrop-blur-lg z-[100] flex items-center border-b-[1px] border-neutral-900 justify-between">
+    <aside className="flex items-center gap-[2px]">
+      <p className="text-3xl font-bold">Auto</p>
+      <Image
+        src="/fuzzieLogo.png"
+        width={15}
+        height={15}
+        alt="automatex logo"
+        className="shadow-sm"
+      />
+      <p className="text-3xl font-bold">matex</p>
+    </aside>
             </header>
 
             
