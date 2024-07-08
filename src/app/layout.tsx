0555c5d61e87d95cba177bd869e3766abed4cb70
emretasss <tas7095@gmail.com>
2024-07-08 09:17:31 +0300

    font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,8 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import {DM_Sans} from "next/font/google"
 import "./globals.css";
 import { ThemeProvider } from "@/providers/theme-provider";
-const inter = Inter({ subsets: ["latin"] });
+const font  = DM_Sans({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -16,7 +16,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body className={font.className}>
       <ThemeProvider
             attribute="class"
             defaultTheme="dark"
