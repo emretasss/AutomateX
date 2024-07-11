dbf7183761eaac4d60cae7845edee7a25d348182
emretasss <tas7095@gmail.com>
2024-07-11 05:26:23 +0300

    "Added newline at end of file in constants.ts"

--- src/lib/constans.ts
+++ src/lib/constans.ts
@@ -1,3 +1,4 @@
-export const clients= [...new Array(10)].map((client,index) => ({
-    href : `/${index+1}.png`,
-}))
+export const clients = [...new Array(10)].map((client, index) => ({
+    href: `/${index + 1}.png`,
+  }))
+  
