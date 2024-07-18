7272689143457c7607d16fb22ce9ebcf81fc18bd
emretasss <tas7095@gmail.com>
2024-07-18 19:40:00 +0300

    "Added dashboard title and styling to DashboardPage component"

--- src/app/(main)/(pages)/dashboard/page.tsx
+++ src/app/(main)/(pages)/dashboard/page.tsx
@@ -4,9 +4,11 @@ import React from 'react'
 
 const DasboardPage = () => {
   return( 
-  <div>
-
-  </div>
+    <div className="flex flex-col gap-4 relative">
+      <h1 className="text-4xl sticky top-0 z-[10] p-6 bg-background/50 backdrop-blur-lg flex items-center border-b">
+        Dashboard
+      </h1>
+    </div>
   )
 }
 
