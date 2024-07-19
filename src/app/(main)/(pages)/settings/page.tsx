5ce071039f7ee4432a343b69a070fbdaaac86301
emretasss <tas7095@gmail.com>
2024-07-19 19:09:45 +0300

    "Updated package-lock.json and package.json to include new dependencies (@hookform/resolvers, @radix-ui/react-label, react-hook-form, and zod); modified Settings page component in page.tsx to include new UI elements"

--- src/app/(main)/(pages)/settings/page.tsx
+++ src/app/(main)/(pages)/settings/page.tsx
@@ -4,7 +4,20 @@ type Props = {}
 
 function Settings({}: Props) {
   return (
-    <div className='flex flex-col gap-4'>Settings</div>
+    <div className="flex flex-col gap-4">
+      <h1 className="sticky top-0 z-[10] flex items-center justify-between border-b bg-background/50 p-6 text-4xl backdrop-blur-lg">
+        <span>Settings</span>
+      </h1>
+      <div className="flex flex-col gap-10 p-6">
+        <div>
+          <h2 className="text-2xl font-bold">User Profile</h2>
+          <p className="text-base text-white/50">
+            Add or update your information
+          </p>
+        </div>
+        </div>
+        </div>
+
   )
 }
 
---
+++ src/components/forms/profile-form.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+type Props = {}
+
+const ProfileForm = (props: Props) => {
+  return (
+    <div>ProfileForm</div>
+  )
+}
+
+export default ProfileForm
