cc11694b3b3c42311eb87be8dc8125cecd545359
emretasss <tas7095@gmail.com>
2024-07-19 04:10:24 +0300

    "Added Lucide icons and separators to sidebar component"

--- src/components/sidebar/index.tsx
+++ src/components/sidebar/index.tsx
@@ -11,6 +11,7 @@ import {
 import { menuOptions } from '@/lib/constant'
 import clsx from 'clsx'
 import { Separator } from "@/components/ui/separator"
+import { Database, GitBranch, LucideMousePointerClick } from 'lucide-react'
 
 
 
@@ -60,10 +61,41 @@ const MenuOptions = (props: Props) => {
             </ul>
           ))}
         </TooltipProvider>
-      
+          <Separator />
     
+          <div className="flex items-center flex-col gap-9 dark:bg-[#353346]/30 py-4 px-2 rounded-full h-56 overflow-scroll border-[1px]">
+          <div className="relative dark:bg-[#353346]/70 p-2 rounded-full dark:border-t-[2px] border-[1px] dark:border-t-[#353346]">
+            <LucideMousePointerClick
+              className="dark:text-white"
+              size={18}
+            />
+            <div className="border-l-2 border-muted-foreground/50 h-6 absolute left-1/2 transform translate-x-[-50%] -bottom-[30px]" />
+          </div>
+          <div className="relative dark:bg-[#353346]/70 p-2 rounded-full dark:border-t-[2px] border-[1px] dark:border-t-[#353346]">
+            <GitBranch
+              className="text-muted-foreground"
+              size={18}
+            />
+            <div className="border-l-2 border-muted-foreground/50 h-6 absolute left-1/2 transform translate-x-[-50%] -bottom-[30px]"></div>
+          </div>
+          <div className="relative dark:bg-[#353346]/70 p-2 rounded-full dark:border-t-[2px] border-[1px] dark:border-t-[#353346]">
+            <Database
+              className="text-muted-foreground"
+              size={18}
+            />
+            <div className="border-l-2 border-muted-foreground/50 h-6 absolute left-1/2 transform translate-x-[-50%] -bottom-[30px]"></div>
+          </div>
+          <div className="relative dark:bg-[#353346]/70 p-2 rounded-full dark:border-t-[2px] border-[1px] dark:border-t-[#353346]">
+            <GitBranch
+              className="text-muted-foreground"
+              size={18}
+            />
+          </div>
+        </div>
       </div>
-    
+      <div className="flex items-center justify-center flex-col gap-8">
+        <ModeToggle />
+      </div>
     </nav>
   )
 }
