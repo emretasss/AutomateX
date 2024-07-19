80dc2715309cd912c085a5b83fbb68712c3e943a
emretasss <tas7095@gmail.com>
2024-07-19 03:03:39 +0300

    "Added imports for icons, updated clients array, and added new exports for menuOptions and EditorCanvasDefaultCardTypes"

--- src/lib/constans.ts
+++ src/lib/constans.ts
@@ -1,8 +1,15 @@
+import Category from '@/components/icons/category'
+import Logs from '@/components/icons/clipboard'
+import Templates from '@/components/icons/cloud_download'
+import Home from '@/components/icons/home'
+import Payment from '@/components/icons/payment'
+import Settings from '@/components/icons/settings'
+import Workflows from '@/components/icons/workflows'
+
 export const clients = [...new Array(10)].map((client, index) => ({
-    href: `/${index + 1}.png`,
-  }))
-  
-  
+  href: `/${index + 1}.png`,
+}))
+
 export const products = [
   {
     title: 'Moonbeam',
@@ -83,3 +90,58 @@ export const products = [
     thumbnail: '/p3.png',
   },
 ]
+
+export const menuOptions = [
+  { name: 'Dashboard', Component: Home, href: '/dashboard' },
+  { name: 'Workflows', Component: Workflows, href: '/workflows' },
+  { name: 'Settings', Component: Settings, href: '/settings' },
+  { name: 'Connections', Component: Category, href: '/connections' },
+  { name: 'Billing', Component: Payment, href: '/billing' },
+  { name: 'Templates', Component: Templates, href: '/templates' },
+  { name: 'Logs', Component: Logs, href: '/logs' },
+]
+
+export const EditorCanvasDefaultCardTypes = {
+  Email: { description: 'Send and email to a user', type: 'Action' },
+  Condition: {
+    description: 'Boolean operator that creates different conditions lanes.',
+    type: 'Action',
+  },
+  AI: {
+    description:
+      'Use the power of AI to summarize, respond, create and much more.',
+    type: 'Action',
+  },
+  Slack: { description: 'Send a notification to slack', type: 'Action' },
+  'Google Drive': {
+    description:
+      'Connect with Google drive to trigger actions or to create files and folders.',
+    type: 'Trigger',
+  },
+  Notion: { description: 'Create entries directly in notion.', type: 'Action' },
+  'Custom Webhook': {
+    description:
+      'Connect any app that has an API key and send data to your applicaiton.',
+    type: 'Action',
+  },
+  Discord: {
+    description: 'Post messages to your discord server',
+    type: 'Action',
+  },
+  'Google Calendar': {
+    description: 'Create a calendar invite.',
+    type: 'Action',
+  },
+  Trigger: {
+    description: 'An event that starts the workflow.',
+    type: 'Trigger',
+  },
+  Action: {
+    description: 'An event that happens after the workflow begins',
+    type: 'Action',
+  },
+  Wait: {
+    description: 'Delay the next action step by using the wait timer.',
+    type: 'Action',
+  },
+}
