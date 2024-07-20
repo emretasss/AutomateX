97576740eb7d5cba51f1c3b66c0ecadbb91e4d0b
emretasss <tas7095@gmail.com>
2024-07-20 04:42:37 +0300

    uploadcare

---
+++ src/app/(main)/(pages)/settings/_components/uploadcare-button.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+type Props = {
+    
+}
+
+const UploadCareButton = (props: Props) => {
+  return (
+    <div>UploadCareButton</div>
+  )
+}
+
+export default UploadCareButton
