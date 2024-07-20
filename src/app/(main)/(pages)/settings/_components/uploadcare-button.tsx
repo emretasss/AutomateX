10fb60b50776a0487e6a82164ff8b241307a14ad
emretasss <tas7095@gmail.com>
2024-07-20 04:58:56 +0300

    "Updated UploadCareButton component to use @uploadcare/blocks and added necessary imports and configurations; updated tsconfig.json to include @uploadcare/blocks types"

--- src/app/(main)/(pages)/settings/_components/uploadcare-button.tsx
+++ src/app/(main)/(pages)/settings/_components/uploadcare-button.tsx
@@ -1,16 +1,47 @@
-"use client"
-import '@uploadcare/react-uploader/core.css';
-
-import React from 'react'
+'use client'
+import React, { useEffect, useRef } from 'react'
+import * as LR from '@uploadcare/blocks'
+import { useRouter } from 'next/navigation'
 
 type Props = {
-    onUpload?: any 
-    
+  onUpload: (e: string) => any
 }
 
-const UploadCareButton = (props: Props) => {
+LR.registerBlocks(LR)
+
+const UploadCareButton = ({ onUpload }: Props) => {
+  const router = useRouter()
+  const ctxProviderRef = useRef<
+    typeof LR.UploadCtxProvider.prototype & LR.UploadCtxProvider
+  >(null)
+
+  useEffect(() => {
+    const handleUpload = async (e: any) => {
+      const file = await onUpload(e.detail.cdnUrl)
+      if (file) {
+        router.refresh()
+      }
+    }
+    ctxProviderRef.current?.addEventListener('file-upload-success', handleUpload)
+  }, [])
+
   return (
-    <div>UploadCareButton</div>
+    <div>
+      <lr-config
+        ctx-name="my-uploader"
+        pubkey="4d67fc35d45847197eaf"
+      />
+
+      <lr-file-uploader-regular
+        ctx-name="my-uploader"
+        css-src={`https://cdn.jsdelivr.net/npm/@uploadcare/blocks@0.35.2/web/lr-file-uploader-regular.min.css`}
+      />
+
+      <lr-upload-ctx-provider
+        ctx-name="my-uploader"
+        ref={ctxProviderRef}
+      />
+    </div>
   )
 }
 
