075edcd2fa8a45cab4fc86344fad6c1a6fd20542
Emre Taş <75886362+emretasss@users.noreply.github.com>
2024-07-24 03:34:43 +0300

    "Update logo image source in Navbar component from ' to 'AutomatexLogo.png'"

--- src/components/global/navbar.tsx
+++ src/components/global/navbar.tsx
@@ -15,7 +15,7 @@ const Navbar = async (props: Props) => {
       <aside className="flex items-center gap-[2px]">
         <p className="text-3xl font-bold">Fu</p>
         <Image
-          src="/fuzzieLogo.png"
+          src="/AutomatexLogo.png"
           width={15}
           height={15}
           alt="fuzzie logo"
