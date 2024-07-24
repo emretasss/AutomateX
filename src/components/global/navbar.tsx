256e50b519b676a9e065e3c18695881714002015
Emre Taş <75886362+emretasss@users.noreply.github.com>
2024-07-24 03:49:11 +0300

    "Update navbar logo and text: rename 'Fu' to 'Auto', 'fuzzie' to 'Automatex', and 'zie' to 'Matex'"

--- src/components/global/navbar.tsx
+++ src/components/global/navbar.tsx
@@ -13,15 +13,15 @@ const Navbar = async (props: Props) => {
   return (
     <header className="fixed right-0 left-0 top-0 py-4 px-4 bg-black/40 backdrop-blur-lg z-[100] flex items-center border-b-[1px] border-neutral-900 justify-between">
       <aside className="flex items-center gap-[2px]">
-        <p className="text-3xl font-bold">Fu</p>
+        <p className="text-3xl font-bold">Auto</p>
         <Image
           src="/AutomatexLogo.png"
           width={15}
           height={15}
-          alt="fuzzie logo"
+          alt="Automatex logo"
           className="shadow-sm"
         />
-        <p className="text-3xl font-bold">zie</p>
+        <p className="text-3xl font-bold">Matex</p>
       </aside>
       <nav className="absolute left-[50%] top-[50%] transform translate-x-[-50%] translate-y-[-50%] hidden md:block">
         <ul className="flex items-center gap-4 list-none">
